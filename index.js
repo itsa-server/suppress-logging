ee695679910071bb9aab9fc2363de05db524192d
Marco Asbreuk <marco@itsasbreuk.nl>
2015-03-16 20:56:59 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var NOOP = function() {};
+
+console._log = console.log;
+console._info = console.info;
+console._warn = console.warn;
+console._error = console.error;
+console._debug = console.debug;
+
+module.exports = {
+    log: function(enableLog) {
+        console.log = enableLog ? console._log : NOOP;
+        console.info = enableLog ? console._info : NOOP;
+        console.warn = enableLog ? console._warn : NOOP;
+        console.error = enableLog ? console._error : NOOP;
+        console.debug = enableLog ? console._debug : NOOP;
+    }
+}
